d230a2c835d92d4936fed8fd4da6e382088cdb0c
dryg <xboxseriaskell@gmail.com>
2023-03-29 00:01:01 +0500

    Домашня Работа на тему Обьекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book Book = new Book("Федор Достоевский ", "Война и мир ", 1856);
+        System.out.println("Книга  = " + Book.getBoocName());
+        System.out.println("Автор = " + Book.getAutorName());
+        System.out.println("Год издания = " + Book.publishingYear);
+        Book.setAuthorName("Дж.К. Роулинг ");
+        // Вызвали сеттер и создадим ноую книгу тут же за счет сеттера.
+        System.out.println("Автор  = " + Book.getAutorName());
+        Book.setBookName("Гарри Поттер ");
+        // Создали новую книгу Гарри Поттер
+        System.out.println("Название книги = " + Book.getBoocName());
+        Book.setPublishingYear(1997);
+        // Создали с помощью Сеттера новый годвыпуска книги
+        System.out.println("Год  издания книги = " + Book.getPublishingYear());
     }
 }
