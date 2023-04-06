5b5b25360cdc66c0c9e324a09f8088a32c06850f
dryg <xboxseriaskell@gmail.com>
2023-04-06 21:06:28 +0500

    Домашня Работа на тему Обьекты и методы

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author("Федор ","Достоевкий");
-      Book book1 = new Book(" Война и мир", 1856, author);
-      Book book2 = new Book(" Война и мир", 1868, author);
+        Author author = new Author("Федор ", "Достоевкий");
+        Book book1 = new Book(" Война и мир", 1856, author);
+        Book book2 = new Book(" Война и мир", 1868, author);
         System.out.println(book2.equals(book1));
     }
 }
